d46d3c387e8e7b68ae0e98fb751c1a41a8ae1efd
Dzank3r <87384409+Dzank3r@users.noreply.github.com>
2022-02-09 23:51:06 +0100

    Add files via upload

---
+++ virtual piano/js.js
@@ -0,0 +1,52 @@
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyA") {
+        let audio = new Audio("white_keys/A.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyS") {
+        let audio = new Audio("white_keys/S.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyD") {
+        let audio = new Audio("white_keys/D.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyF") {
+        let audio = new Audio("white_keys/F.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyG") {
+        let audio = new Audio("white_keys/G.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyH") {
+        let audio = new Audio("white_keys/H.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyJ") {
+        let audio = new Audio("white_keys/J.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyW") {
+        let audio = new Audio("white_keys/J.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyE") {
+        let audio = new Audio("white_keys/J.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyT") {
+        let audio = new Audio("white_keys/J.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyY") {
+        let audio = new Audio("white_keys/J.mp3")
+        audio.play();
+    }
+    else if (event.code == "KeyU") {
+        let audio = new Audio("white_keys/J.mp3")
+        audio.play();
+    }
+    else
+        console.log("warning")
+});
